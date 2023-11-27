f7aabade6b01e8efde149b3628fcf83419a445d6
Ubuntu User <ubuntu@BeagleBone-AI64.localdomain>
2023-11-27 21:27:11 +0000

    PWM demo that is missing PWM outputs for all control inputs, only has one

--- src/demo_leds/src/subscriber_member_function.cpp
+++ src/demo_leds/src/subscriber_member_function.cpp
@@ -36,56 +36,78 @@ public:
 private:
 
 	
+    	 	std::string pwm0_enable = "/sys/class/pwm/pwmchip0/pwm0/enable";
+		std::string pwm0_duty_cycle = "/sys/class/pwm/pwmchip0/pwm0/duty_cycle";
+		std::string pwm0_period = "/sys/class/pwm/pwmchip0/pwm0/period";
+		
+		std::string pwm1_enable = "/sys/class/pwm/pwmchip0/pwm1/enable";
+		std::string pwm1_duty_cycle = "/sys/class/pwm/pwmchip0/pwm1/duty_cycle";
+		std::string pwm1_period = "/sys/class/pwm/pwmchip0/pwm0/period";
+
+
+		
     	 	std::string output_file0 = "/sys/class/leds/beaglebone:green:usr0/brightness";
 		 std::string output_file1 = "/sys/class/leds/beaglebone:green:usr1/brightness";
 		 std::string output_file2 = "/sys/class/leds/beaglebone:green:usr2/brightness";
 		 std::string output_file3 = "/sys/class/leds/beaglebone:green:usr3/brightness";
 		 std::string output_file4 = "/sys/class/leds/beaglebone:green:usr4/brightness";
-		
 
   void topic_callback(const std_msgs::msg::String & msg) const
   {
 
-	std::ofstream file0(output_file0, std::ios::app);
+		std::ofstream file0(output_file0, std::ios::app);
                 std::ofstream file1(output_file1, std::ios::app);
                 std::ofstream file2(output_file2, std::ios::app);
                 std::ofstream file3(output_file3, std::ios::app);
                 std::ofstream file4(output_file4, std::ios::app);
 
+		std::ofstream pwm0_en(pwm0_enable, std::ios::app);
+		std::ofstream pwm0_dut(pwm0_duty_cycle, std::ios::app);
+		std::ofstream pwm0_per(pwm0_period, std::ios::app);
+		
+		std::ofstream pwm1_en(pwm1_enable, std::ios::app);
+                std::ofstream pwm1_dut(pwm1_duty_cycle, std::ios::app);
+                std::ofstream pwm1_per(pwm1_period, std::ios::app);
+		
+		pwm0_en << 1;
+		pwm1_en << 1;
+
   	if(std::stoi(msg.data) == 0){
   	
+		pwm0_dut << 500;
+		pwm0_per << 1000;
 
+		pwm1_dut << 500;
+                pwm1_per << 1000;
 	  	
 	  	file0 << 1;
 		file1 << 0;
                 file2 << 0;
                 file3 << 0;
                 file4 << 0;
-	  	
 	    	RCLCPP_INFO(this->get_logger(), "Setting servo to position: '%s'", msg.data.c_str());
     
     	}
     	
     	if(std::stoi(msg.data) == 1){
   	
-                file0 << 0;
+
+	    	 file0 << 0;
                 file1 << 1;
                 file2 << 0;
                 file3 << 0;
                 file4 << 0;
-
-	    	RCLCPP_INFO(this->get_logger(), "Setting servo to position: '%s'", msg.data.c_str());
+		RCLCPP_INFO(this->get_logger(), "Setting servo to position: '%s'", msg.data.c_str());
     
     	}
     	
     	if(std::stoi(msg.data) == 2){
   	  	
-                file0 << 0;
+	  	file0 << 0;
                 file1 << 0;
                 file2 << 1;
                 file3 << 0;
                 file4 << 0;
-	  	
 	  	
 	    	RCLCPP_INFO(this->get_logger(), "Setting servo to position: '%s'", msg.data.c_str());
     
@@ -93,13 +115,6 @@ private:
     	
     	if(std::stoi(msg.data) == 3){
   	
-	  
-                file0 << 0;
-                file1 << 0;
-                file2 << 0;
-                file3 << 1;
-                file4 << 0;
-
 	  	
 	  	
 	    	RCLCPP_INFO(this->get_logger(), "Setting servo to position: '%s'", msg.data.c_str());
@@ -107,18 +122,16 @@ private:
     	}
 	if(std::stoi(msg.data) == 4){
 
-                file0 << 0;
+		 file0 << 0;
                 file1 << 0;
                 file2 << 0;
-                file3 << 0;
-                file4 << 1;
-
-
-
+                file3 << 1;
+                file4 << 0;
                 RCLCPP_INFO(this->get_logger(), "Setting servo to position: '%s'", msg.data.c_str());
 
         }
   }
+
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
 
