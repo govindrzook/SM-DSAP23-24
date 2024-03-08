47bc5b58af5fad0e89b6002cb888c813da0b5f06
thlgrc <77205277+thlgrc@users.noreply.github.com>
2024-03-07 22:08:17 -0400

    Update FrontRightServoController.cpp
    changed private functions to have %s and msg.data.c_str()

--- src/demo_leds/src/FrontRightServoController.cpp
+++ src/demo_leds/src/FrontRightServoController.cpp
@@ -28,12 +28,12 @@ private:
 
   void steering_position_callback(const std_msgs::msg::String & msg) const
   {
-	    	RCLCPP_INFO(this->get_logger(), "Steering position: '%f'", msg.data);  
+	    	RCLCPP_INFO(this->get_logger(), "Steering position: '%s'", msg.data.c_str());  
 		    
   }
  void brake_position_callback(const std_msgs::msg::String & msg) const
   {
-	    	RCLCPP_INFO(this->get_logger(), "Brake position: '%f'", msg.data);  
+	    	RCLCPP_INFO(this->get_logger(), "Brake position: '%s'", msg.data.c_str());  
 		    
   }
  
