bd9fae9f4dc538ba3a73f98bd27b90b7dafed8e8
thlgrc <epadilla@unb.ca>
2024-03-30 12:34:35 -0300

    created a timer_ask_callback

--- src/demo_leds/src/CentralController.cpp
+++ src/demo_leds/src/CentralController.cpp
@@ -13,7 +13,7 @@
 
 using std::placeholders::_1;
 int flag = 1;
-int key;
+int key = 5;
 int speed = 0;
 int steer = 155;
 int brake = 130;
@@ -48,12 +48,66 @@ public:
    	pub2_ = create_publisher<std_msgs::msg::UInt8>("frontRightBrakePosition", 10);
 	pub3_ = create_publisher<std_msgs::msg::Float64>("frontRightTorque", 10);
 
-	// timer_ = create_wall_timer(std::chrono::seconds(5), std::bind(&CentralController::timer_callback, this));
-	// timer_steer = create_wall_timer(std::chrono::seconds(1), std::bind(&CentralController::timer_steer_callback, this));
+	timer_ = create_wall_timer(std::chrono::seconds(5), std::bind(&CentralController::timer_callback, this));
+	timer_ask = create_wall_timer(std::chrono::seconds(1), std::bind(&CentralController::timer_ask_callback, this));
 
   }
 
 private:
+
+	void timer_ask_callback{
+		//  while(flag){
+			printf("Enter '1' if you want to change the speed.\n");
+			printf("Enter '2' if you want to change the the steering angle.\n");
+			printf("Enter '3' if you want to change the set the braking angle.\n");
+			printf("Enter '4' if you want to display all the output values.\n");
+			printf("Enter '0' if you want to stop\n");
+			scanf(" %d",&key);
+			
+			switch(key){
+				case 0:
+					flag = 0;
+					printf("STOP\n.")
+					rclcpp::shutdown();
+					break;
+
+				case 1:
+					printf("Please enter the speed in RPM [min - max]\n.");
+					scanf(" %d",&speed);
+					printf("Speed: %d\n",speed);
+
+					// rclcpp::spin_some(std::make_shared<CentralController>());
+					break;
+				
+				case 2:
+					printf("Please enter the steering angle [130 to 180]\n.");
+					scanf(" %d",&steer);
+					printf("Steer: %d\n",steer);
+					// rclcpp::spin_some(std::make_shared<CentralController>());
+					break;
+
+				case 3:
+					printf("Please enter the braking angle [40 to 130]\n.");
+					scanf(" %d",&brake);
+					printf("Brake: %d\n",brake);
+					// rclcpp::spin_some(std::make_shared<CentralController>());
+					break;
+				
+				case 4:
+					printf("OUTPUTS\n.");
+					// rclcpp::spin_some(std::make_shared<CentralController>());
+					break;
+
+				default:
+					printf("Invalid key. Please try again.\n");
+					break;
+			}
+			
+
+
+//   }
+		
+	}
 
 	void timer_callback(){
 
@@ -155,7 +209,7 @@ private:
 	rclcpp::Publisher<std_msgs::msg::UInt8>::SharedPtr pub1_;
 	rclcpp::Publisher<std_msgs::msg::UInt8>::SharedPtr pub2_;
 	rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr pub3_;
-  	// rclcpp::TimerBase::SharedPtr timer_;
+  	rclcpp::TimerBase::SharedPtr timer_;
 	// rclcpp::TimerBase::SharedPtr timer_steer;
 
 };
@@ -164,49 +218,7 @@ int main(int argc, char * argv[])
 {
 
   rclcpp::init(argc, argv);
-  while(flag){
-	printf("Enter '1' if you want to change the speed.\n");
-	printf("Enter '2' if you want to change the the steering angle.\n");
-	printf("Enter '3' if you want to change the set the braking angle.\n");
-	printf("Enter '4' if you want to display all the output values.\n");
-	printf("Enter '0' if you want to stop\n");
-	scanf(" %d",&key);
-	switch(key){
-		case 0:
-			flag = 0;
-			break;
-
-		case 1:
-			printf("Please enter the speed in RPM [min - max]\n.");
-			scanf(" %d",&speed);
-
-			rclcpp::spin_some(std::make_shared<CentralController>());
-			break;
-		
-		case 2:
-			printf("Please enter the steering angle [130 to 180]\n.");
-			scanf(" %d",&steer);
-			rclcpp::spin_some(std::make_shared<CentralController>());
-			break;
-
-		case 3:
-			printf("Please enter the braking angle [40 to 130]\n.");
-			scanf(" %d",&brake);
-			rclcpp::spin_some(std::make_shared<CentralController>());
-			break;
-		
-		case 4:
-			printf("OUTPUTS\n.");
-			rclcpp::spin_some(std::make_shared<CentralController>());
-			break;
-
-		default:
-			printf("Invalid key. Please try again.\n");
-			break;
-	}
-
-	
-  }
+  rclcpp::spin(std::make_shared<CentralController>());
   rclcpp::shutdown();
   return 0;
 }
