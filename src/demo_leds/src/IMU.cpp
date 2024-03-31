5378b46316d259ec7dd5a3aec5778a37f8986da0
elegresl <115160984+elegresl@users.noreply.github.com>
2024-03-31 11:21:47 -0300

    Update IMU.cpp
    Changed IMU timer

--- src/demo_leds/src/IMU.cpp
+++ src/demo_leds/src/IMU.cpp
@@ -39,7 +39,7 @@ public:
     pub7_ = create_publisher<std_msgs::msg::Float64>("temp", 10);
 
     // Create a timer to update the topics every 1 ms
-    timer_ = create_wall_timer(std::chrono::milliseconds(500), std::bind(&IMU::timer_callback, this));
+    timer_ = create_wall_timer(std::chrono::seconds(1), std::bind(&IMU::timer_callback, this));
     
   }
 
